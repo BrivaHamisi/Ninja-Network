9127fd6c11ae39f744d5ccdd372ce1dcc36ce502
BrivaHamisi <brivahamisi@gmail.com>
2024-11-15 20:31:16 +0300

    Views and Routes

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ninja Network</title>
+    <title>Ninja Network | Home</title>
 </head>
 <body>
-    <h1>Welcome to the Ninja Network</h1>
-    <p>Click the Button below to View the List of Ninjas</p>
+    <h2>Currently available Ninjas</h2>
+    <ul>
+        <li>Ninjas Here</li>
+    </ul>
 
-    <a href="/ninjas" class="btn">Find Ninjas</a>
+    <a href="" class="btn">Find Ninjas</a>
 </body>
 </html>
