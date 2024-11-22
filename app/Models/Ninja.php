beaa522779d79823a8311cf2b01f7f25ad861e2b
BrivaHamisi <brivahamisi@gmail.com>
2024-11-22 13:26:43 +0300

    Eloquent Models

---
+++ app/Models/Ninja.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ninja extends Model
+{
+    protected $fillable = [
+        'name',
+        'skills',
+        'bio'
+    ];
+
+    /** @use HasFactory<\Database\Factories\NinjaFactory> */
+    use HasFactory;
+}
